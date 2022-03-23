904de4523058aeb51af28b6c345b3a14eff0cb4d
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-03-23 19:02:21 -0400

    Styling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { engine } = require('express-handlebars');
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
+app.use(express.static('public'));
 
 app.get('/', 
   (req, res) => {
